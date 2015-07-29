dd5e7bdb878d839146f0cb8b3e5ce425d984e0e1
Paul Tagliamonte <tag@pault.ag>
2015-07-28 20:05:03 -0400

    Start to fiddle with ideas.

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+
+	"golang.org/x/exp/inotify"
+	"pault.ag/go/debian/control"
+)
+
+func main() {
+	watchDirectory(".")
+}
+
+func watchDirectory(path string) error {
+	watcher, err := inotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+
+	err = watcher.Watch(path)
+	if err != nil {
+		return err
+	}
+
+	for ev := range watcher.Event {
+		if ev.Mask != 0x08 { // IN_CLOSE_WRITE
+			continue
+		}
+		if !strings.HasSuffix(ev.Name, ".changes") {
+			continue
+		}
+		f, err := os.Open(ev.Name)
+		if err != nil {
+			log.Printf("%s\n", err)
+			continue
+		}
+		para, err := control.ParseParagraph(bufio.NewReader(f))
+		if err != nil {
+			log.Printf("%s\n", err)
+			continue
+		}
+		log.Printf("Upload for: %s\n", para.Values["Distribution"])
+	}
+
+	return nil
+}
