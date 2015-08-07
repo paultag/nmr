0d1169cd2f208a48b5e71e0fb2c164c9d924cbb5
Paul Tagliamonte <tag@pault.ag>
2015-08-07 11:18:15 -0400

    Reflow to be a log hook

--- main.go
+++ main.go
@@ -5,66 +5,36 @@ import (
 	"os"
 
 	"pault.ag/go/debian/control"
-	"pault.ag/go/fancytext"
+	// "pault.ag/go/fancytext"
 	"pault.ag/go/nmr/helpers"
-	"pault.ag/go/nmr/repo"
 )
 
 func main() {
 	arch := "amd64"
-	suite := "unstable"
 	repoRoot := "/home/tag/tmp/repo/"
 
-	fmt.Printf(`
-
-		░█▀█░█▀█░█░█░█▀▀░█▀▄░░░█▄█░█▀█░█░░░█▀▀░░░█▀▄░█▀█░▀█▀
-		░█░█░█▀█░█▀▄░█▀▀░█░█░░░█░█░█░█░█░░░█▀▀░░░█▀▄░█▀█░░█░
-		░▀░▀░▀░▀░▀░▀░▀▀▀░▀▀░░░░▀░▀░▀▀▀░▀▀▀░▀▀▀░░░▀░▀░▀░▀░░▀░
-
-				Arch:    %s
-				Suite:   %s
-				Chroot:  %s
-				Repo:    %s
-
-`, arch, suite, suite, repoRoot)
-
-	fmt.Printf("Loading package indexes and computing build needed...\n")
-
-	needsBuild := map[string]repo.BuildStatus{}
-	var needsBuildList = []repo.BuildStatus{}
-
-	if IsArchAllArch(repoRoot, arch) {
-		needsBuildList = append(
-			GetBuildNeeding(repoRoot, suite, "all"),
-			GetBuildNeeding(repoRoot, suite, arch)...,
-		)
-	} else {
-		needsBuildList = append(GetBuildNeeding(repoRoot, suite, arch))
+	params := os.Args[1:]
+	log, err := ParseLine(repoRoot, params)
+	if err != nil {
+		panic(err)
 	}
 
-	for _, entry := range needsBuildList {
-		needsBuild[entry.Package.Location] = entry
+	if log.Action != "accepted" {
+		return
 	}
 
-	fmt.Printf("  %d packages need build\n", len(needsBuild))
-	fmt.Printf("\n\n")
-
-	for _, pkg := range needsBuild {
-		dscPath := repoRoot + "/" + pkg.Package.Location
-
-		if pkg.Buildable {
-			BuildPackage(dscPath, arch, suite, repoRoot, false)
-			fmt.Printf("      built %s!\n", dscPath)
-		} else {
-			fmt.Printf("Package %s unbuildable currently\n", pkg.Package.Location)
-			fmt.Printf("      %s\n", pkg.Why)
-		}
+	// Right, let's run a build.
+	dsc, err := log.Changes.GetDSC()
+	if err != nil {
+		panic(err)
 	}
+
+	BuildPackage(dsc.Filename, arch, log.Suite, repoRoot, true)
 }
 
 func BuildPackage(dscFile, arch, suite, repoRoot string, verbose bool) {
-	done := fancytext.FormatSpinner(fmt.Sprintf("%%s  -  building %s", dscFile))
-	defer done()
+	// done := fancytext.FormatSpinner(fmt.Sprintf("%%s  -  building %s", dscFile))
+	// defer done()
 
 	incomingLocation, err := GetIncoming(repoRoot, suite)
 	if err != nil {
